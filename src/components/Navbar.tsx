4a12826531516ff873161a122196c94b2d252a77
Davinder9297 <davindergiri765@gmail.com>
2025-06-14 10:10:23 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='text-red-500 hover:text-blue-600 font-extrabold'>
+      Hi i am navbar
+    </div>
+  )
+}
+
+export default Navbar
