319c3a3bf807d8a56d78fa9d3800abf64f939bc5
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2025-06-18 00:14:22 +0530

    feat: extra components removed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,9 +4,8 @@ import HowItWorks from "@/components/home/howItWorks"
 export default function Home() {
   return (
     <div className="p-10">
-      <div><HowItWorks /> </div>
-     
-<div><ExclusiveServices/>
+      <div className="pt-12 md:pt-24"><HowItWorks /> </div>     
+<div className="pt-12 md:pt-24"><ExclusiveServices/>
 </div>   
     </div>
   )
